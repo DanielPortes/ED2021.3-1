40e31e45be2a1a088f330d8de12e4820577a8d8e
ArthurUFJF <arthurmpereira2010@gmail.com>
2022-01-13 13:57:08 -0300

    finalizacao do bench e da impressao

--- src/arvoreVP.h
+++ src/arvoreVP.h
@@ -284,24 +284,74 @@ void inicializaVetorAleatorioArvore(vector<elementoArvore> &elemento, int size)
         elemento[j].posicaoBinario=j;
     }
 }
+void imprimeResultadoArvoreVP(int comparacoesMedias,int comparacoesMediasBusca,double tempoMedioInsercao,double tempoMedioBusca,int mediaAchados){
+   
+    ofstream arquivotxt("saida.txt",ios::app);
+    arquivotxt << "***********************************************************************"  << endl;
+    arquivotxt << "**********************   Arvore Vermelho - Preto  *********************" << endl;
+    arquivotxt << "***********************************************************************" <<  endl;
+    arquivotxt << "Numero de comparações médias para inserção de 1.000.000 elementos:" << comparacoesMedias <<  endl;
+    arquivotxt << "Tempo médio para inserção de 1.000.000 elementos:" << tempoMedioInsercao <<  endl;
+    arquivotxt << "Numero de comparações médias para busca de 100 elementos:" << comparacoesMediasBusca <<  endl;
+    arquivotxt << "Tempo médios para busca de 100 elementos:" << tempoMedioBusca <<  endl;
+    arquivotxt << "Numero médio de reviews encontradas na busca para busca de 100 elementos:" << mediaAchados <<  endl;
+    arquivotxt << "***********************************************************************"  << endl;
+    arquivotxt.close();
 
+    
+
+}
 
 
 void benchmarkArvoreVP(){
-    vector<elementoArvore> elementos;
-    inicializaVetorAleatorioArvore(elementos,A); //inicializando vetor de elementos
     int comparacoes=0;
     int comparacoesBusca=0;
     int entrada=0;
+    int achados=0;
     bool conseguiu=false;
     elementoArvore aux;
+    vector <elementoArvore> elementosBusca;
     string id;
     arvoreVP arvore;
-    for (int i = 0; i <A;i++){  //inserção
-        arvore.inserir(&elementos[i],&comparacoes);
+    vector<elementoArvore> elementos;
+    clock_t h;        // Criando variaveis de tempo
+    h = clock();      // Iniciando o tempo 
+    cout << "inicializando inserção..." <<endl;
+    for(int i=0;i<3;i++){   //rodando 3 vezes para tirar media
+        cout << "inserção dos 1.000.000 elementos,teste numero " <<i+1<<" de 3"<<endl;
+        inicializaVetorAleatorioArvore(elementos,A); //inicializando vetor de elementos
+        for (int i = 0; i <A;i++){  //inserção
+            arvore.inserir(&elementos[i],&comparacoes);
+        }
     }
-    cout <<"inserção concluida,com numero de comapracoes igua a:"<<comparacoes<<endl;
+    h = clock() - h;  // Finalizando o tempo
+    h=h/3;
+    double auxTempoInsere = ((double)h)/((CLOCKS_PER_SEC)); 
+    comparacoes=comparacoes/3;
 
+    //-------busca--------
+    cout << "Realizando testes de busca..." <<endl;
+    h = clock();      // Iniciando o tempo 
+    for(int i = 0; i < 3;i++){
+        cout << "busca dos 100 elementos,teste numero " <<i+1<<" de 3"<<endl;
+        inicializaVetorAleatorioArvore(elementosBusca,100); 
+        for(int j = 0; j < 100 ;j++){
+           conseguiu=0;
+            aux=arvore.buscar(id,&comparacoesBusca,&conseguiu);
+            if(conseguiu==true){
+                achados++;
+            }
+        }
+    }
+    h = clock() - h;  // Finalizando o tempo
+    h=h/3;
+    double auxTempoBusca = ((double)h)/((CLOCKS_PER_SEC)); 
+    achados=achados/3;
+    cout << "Realizando impressão..." <<endl;
+    imprimeResultadoArvoreVP(comparacoes,comparacoesBusca,auxTempoInsere,auxTempoBusca,achados);
+    cout << "benchmark concluido com sucesso" <<endl;
+    
+    /*--------------TESTE MANUAL DE BUSCA--------------------------------------------------
     while(true){ //busca 
         cout<<"deseja procurar uma id?  1-sim || 2-nao"<<endl;
         cin>>entrada;
@@ -330,7 +380,7 @@ void benchmarkArvoreVP(){
         else{
             return;
         }
-    }
+    }*/
 }
 
 
