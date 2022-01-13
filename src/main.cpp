eaaefe17846fe33179b2351b79a98fd3b09993b6
DanielPortes <fagundes.daniel191@gmail.com>
2022-01-13 17:51:08 -0300

    ,

--- src/main.cpp
+++ src/main.cpp
@@ -12,13 +12,13 @@ using namespace std;
 
 enum EscolhasChamada
 {
-    lerCSV = 'l',
-    escreverBinario = 'e',
-    modulo_de_teste = 't',
-    ordenacao = 'o',
-    sair = 's',
-    arvoreVerPre = 'v'
-    tabela_Hash = 'h'
+	lerCSV = 'l',
+	escreverBinario = 'e',
+	modulo_de_teste = 't',
+	ordenacao = 'o',
+	sair = 's',
+	arvoreVerPre = 'v',
+	tabela_Hash = 'h'
 };
 
 void menu(const string& caminhoEntrada, vector<Review>& reviews)
@@ -31,53 +31,55 @@ void menu(const string& caminhoEntrada, vector<Review>& reviews)
 		cout << "\tDigite 'e' para: escreverBinario()\n";
 		cout << "\tDigite 'o' para: ordenacao() \n";
 		cout << "\tDigite 'h' para: tabelaHash() \n";
-        cout << "\tDigite 'v' para: arvoreVP() ,recomendamos sempre ler e gerar o bin no inicio das rodadas de testes \n";
+		cout <<
+			"\tDigite 'v' para: arvoreVP() ,recomendamos sempre ler e gerar o bin no inicio das rodadas de testes \n";
 		cout << "\tDigite 't' para: Modulo de teste \n";
 		cout << "\tDigite 's' para sair " << endl;
 
 		char entrada = '\0';
 		cin >> entrada;
 
-        switch (entrada)
-        {
-            case lerCSV:
-            {
-                {
-                    Timer timer("Tempo para lerArquivoCSV()");
-                    lerArquivoCSV(caminhoEntrada, reviews);
-                    break;
-                }
-            }
-            case escreverBinario:
-            {
-                {
-                    Timer timer("Tempo para escreveBin():");
-                    escreverSaidaBinaria(reviews);
-                    break;
-                }
-            }
-            case modulo_de_teste:
-            {
-                moduloTeste();
-                break;
-            }
-            case ordenacao:
-            {   // os algoritmos de sorting estao todos em ordenacao.cpp, mas os de benchmark foi realizado em Timer para aproveitamento da classe
-                {
-                    Timer timer("HeapSort");
-                    timer.benchHeapSort(3);
-                }
-                {
-                    Timer timerQuick("QuickSort");
-                    timerQuick.benchQuickSort(3);
-                }
-                {
-                    Timer timerComb("CombSort");
-                    timerComb.benchCombSort(3);
-                }
-                break;
-            }
-            		case tabela_Hash:
+		switch (entrada)
+		{
+		case lerCSV:
+			{
+				{
+					Timer timer("Tempo para lerArquivoCSV()");
+					lerArquivoCSV(caminhoEntrada, reviews);
+					break;
+				}
+			}
+		case escreverBinario:
+			{
+				{
+					Timer timer("Tempo para escreveBin():");
+					escreverSaidaBinaria(reviews);
+					break;
+				}
+			}
+		case modulo_de_teste:
+			{
+				moduloTeste();
+				break;
+			}
+		case ordenacao:
+			{
+				// os algoritmos de sorting estao todos em ordenacao.cpp, mas os de benchmark foi realizado em Timer para aproveitamento da classe
+				{
+					Timer timer("HeapSort");
+					timer.benchHeapSort(3);
+				}
+				{
+					Timer timerQuick("QuickSort");
+					timerQuick.benchQuickSort(3);
+				}
+				{
+					Timer timerComb("CombSort");
+					timerComb.benchCombSort(3);
+				}
+				break;
+			}
+		case tabela_Hash:
 			{
 				cout << "Digite quantos reviews aleatorios devem ser importados: ";
 				int input = 0; // nao representa tamanho real da tabela, m_tam = N * 1.x
@@ -88,43 +90,46 @@ void menu(const string& caminhoEntrada, vector<Review>& reviews)
 				imprimeNMaisFrequentes(populares, input);
 				break;
 			}
-            case arvoreVerPre:
-            {
-                cout<<"qual opcao deseja?\n1-modo de analise\n2-busca manual"<<endl;
-                int opcaovp;
-                cin>>opcaovp;
-                switch(opcaovp){
-                    case 1:{
-                        benchmarkArvoreVP();
-                        break;
-                    }
-                    case 2:{
-                        testArvoreVP();
-                        break;
-                    }
-                    default:{
-                        cout<<"opcao invalida"<<endl;
-                        break;
-                    }
-                }
-                
-                break;
-            }
-            case sair:
-            {
-                return;
-            }
-            default:
-            {
-                break;
-            }
-        }
-    }
+		case arvoreVerPre:
+			{
+				cout << "qual opcao deseja?\n1-modo de analise\n2-busca manual" << endl;
+				int opcaovp;
+				cin >> opcaovp;
+				switch (opcaovp)
+				{
+				case 1:
+					{
+						benchmarkArvoreVP();
+						break;
+					}
+				case 2:
+					{
+						testArvoreVP();
+						break;
+					}
+				default:
+					{
+						cout << "opcao invalida" << endl;
+						break;
+					}
+				}
+
+				break;
+			}
+		case sair:
+			{
+				return;
+			}
+		default:
+			{
+				break;
+			}
+		}
+	}
 }
 
 int main(int argc, char* argv[])
 {
-
 	srand(static_cast<unsigned int>(time(nullptr)));
 
 	vector<Review> reviews;
